6fe03fe149245eb98fc7fd18cab3b2bb4e1e8757
isi-code <irvinjose.silva@gmail.com>
2026-01-09 10:29:13 -0800

    Refactor `TodoListHandler`
    Add methods to remove, update task status and task information in localStorage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import { Task } from "./todo.js";
-import { todoListHandler } from "./todoListHandler.js";
+import { Task, TodoListHandler } from "./todo.js";
+import { TaskDom } from "./todo-dom.js"
 import todoList from './todo-list.json'
 
+const pageContContainer = document.getElementById("content");
 
-const test = new Task ("Dance all night long","Check soil moisture for the","2026-01-06","Low");
+class TodoListApp {
+    constructor(container, todoListContent = '[]'){
+        this.pageContContainer = container; 
+        this.todoList = new TodoListHandler(todoListContent);
+        //this.taskDom = new TaskDom(this.todoList.allTasks);
+    }
+}
 
-const addTest = new todoListHandler("todo-list", JSON.stringify(todoList));
-
-addTest.addTask(test);
-
+//const testTask = new Task ("Dance Disco Music","Dance the night away","2026-01-06","Low");
+new TodoListApp(pageContContainer, JSON.stringify(todoList));
--- src/todo.js
+++ src/todo.js
@@ -8,3 +8,39 @@ export class Task {
         this.project = project;
     }
 }
+
+export class TodoListHandler {
+    constructor(content, listName = "todo-list") {
+        this.listName = listName;
+        this.todoList = this.initTodoList(content);   
+    }
+
+    initTodoList(content){
+        if(!(localStorage.key(this.listName) ?? false)) 
+            localStorage.setItem(this.listName, content);
+
+        return JSON.parse(localStorage.getItem(this.listName))
+    }
+
+    get allTasks() { return this.todoList }
+
+    addTask(task) {
+        this.todoList.push(task);
+        localStorage.setItem(this.listName, JSON.stringify(this.todoList));
+    }
+
+    removeTask (taskIdx) {
+        this.todoList.splice(taskIdx, 1);
+        localStorage.setItem(this.listName, JSON.stringify(this.todoList));
+    }
+
+    updateTaskStatus(taskIdx, status) {
+        this.todoList[taskIdx].status = (status === false) ? true : false;
+        localStorage.setItem(this.listName, JSON.stringify(this.todoList));
+    }
+
+    editTaskInfo(taskIdx, field, info) {
+        this.todoList[taskIdx][field] = info;
+        localStorage.setItem(this.listName, JSON.stringify(this.todoList));
+    }
+}
